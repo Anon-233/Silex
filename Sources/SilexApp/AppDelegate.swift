84cd67f162c0a936faba32811dcd67d92488b60a
Anon-233 <105512649+Anon-233@users.noreply.github.com>
2026-06-25 03:23:27 +0800

    fix: add UNUserNotificationCenterDelegate to show notifications in foreground

--- Sources/SilexApp/AppDelegate.swift
+++ Sources/SilexApp/AppDelegate.swift
@@ -1,5 +1,22 @@
 import AppKit
+import UserNotifications
 
 final class AppDelegate: NSObject, NSApplicationDelegate {
-    func applicationDidFinishLaunching(_ notification: Notification) {}
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        UNUserNotificationCenter.current().delegate = NotificationDelegate.shared
+        NSApp.windows.first { $0.title == "Silex" }?.orderOut(nil)
+    }
+}
+
+final class NotificationDelegate: NSObject, UNUserNotificationCenterDelegate {
+    static let shared = NotificationDelegate()
+
+    func userNotificationCenter(
+        _ center: UNUserNotificationCenter,
+        willPresent notification: UNNotification,
+        withCompletionHandler completionHandler:
+            @escaping (UNNotificationPresentationOptions) -> Void
+    ) {
+        completionHandler([.banner, .sound])
+    }
 }
